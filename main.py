b08f3fefef427d160f79f53429b457cff39dc466
Ryan Jesus Valentim de Oliveira <ryan.jesus@hotmail.com>
2022-07-20 10:00:52 -0300

    Fazendo o python abrir os 6 arquivos em excel para analise, usando o pandas e openyxl

---
+++ main.py
@@ -0,0 +1,14 @@
+# pandas
+# openyxl
+# twilio
+
+
+# Passo a passo de solução
+
+# Abrir os 6 arquivos em Excel
+
+# Para cada arquivo:
+# Verificar se algum valor na coluna venda do arquivo é maior que 55.000
+# Se for maior do que 55.000 Enviar um sms com o Nome, o mês e as vendas dele
+
+# Caso não seja maior do que 55.000 não quero fazer nada
